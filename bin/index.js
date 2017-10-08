5e0b661240202ac0d9b4f9a140b037a9beed0494
Sawaruk Sirimungklanurak <sawaruk_sir@ascendinter.com>
2017-10-08 23:25:04 +0700

    use es6

--- bin/index.js
+++ bin/index.js
@@ -13,71 +13,104 @@ var inquirer = require('inquirer');
 
 const RESOURCE_URL = 'https://raw.githubusercontent.com/OnagiNagao/terraform_template/master/';
 
+function getSourceTemplate (fileUrl, encoding) {
+    return new Promise((resolve, reject) => {
+        request({url: fileUrl, encoding: encoding}, (err, res, body) => {
+            if (err) reject(err);
+            resolve(body);
+        });
+    });
+}
+
+function writeSourceTemplate (filePath, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, content, (err) => { if (err) reject(err); resolve()});
+    });
+}
+
+async function downloadTemplate(fileUrl, directory, fileName) {
+    let content = await getSourceTemplate(fileUrl, null);
+    fs.mkdir(directory, (err) => { if (err) throw (err);});
+    await writeSourceTemplate(directory + '/' + fileName, content);
+}
+
+function readPromptFie(promptFilePath) {
+    return new Promise((resolve, reject) => {
+        fs.access(promptFilePath, fs.constants.R_OK, (err) => {
+            if (err) {
+                resolve([]);
+                return;
+            };
+            resolve(require(promptFilePath));
+        });
+    });
+}
+
+function writeTfvars(filePath, answers, projectName) {
+    let content = '';
+    for( key  in answers ){
+        content += (key + '=' + '"' + answers[key] + '"')
+        content += '\r\n';
+    }
+    console.log(content);
+
+    fs.writeFile(filePath, content, (err) => {
+        if (err) return console.log(err);
+        console.log('... write configuration file success');
+        console.log(chalk.green("Init Project: " + projectName + " SUCCESS"));
+    });
+}
+
 program
     .command('init <projectName>')
     .description('init terraform project')
     .option('-m, --module <module>', 'The AWS\'s module', /^(ec2)$/i)
     // .option('-m, --module <module>', 'The AWS\'s module', /^(ec2|apiGateWay)$/i)
-    .action(function(projectName, options) {
-        console.log(chalk.rgb(0,255,174)(
-            figlet.textSync('#  TTerraform  #', { horizontalLayout: 'fitted' })
-        ));
+    .action(async function(projectName, options) {
+        try {
+            //TODO:: refactor variable
+            console.log(chalk.rgb(0, 255, 174)(
+                figlet.textSync('#  TTerraform  #', {horizontalLayout: 'fitted'})
+            ));
 
-        console.log("Init Project:", projectName);
-        if (options.module == true) {
-            console.log(chalk.red('Error: invalid module name'));
-            return;
-        }else if (options.module == undefined){
-            options.module = 'new';
-        }
+            console.log("Init Project:", projectName);
+            if (options.module == true) {
+                console.log(chalk.red('Error: invalid module name'));
+                return;
+            } else if (options.module == undefined) {
+                options.module = 'new';
+            }
 
-        var directoryPrefix = projectName;
-        console.log("Init module:", options.module);
+            var directoryPrefix = projectName;
+            console.log("Init module:", options.module);
 
-        var fileUrl = RESOURCE_URL+options.module+".zip";
-        var output = options.module + ".zip";
-        request({url: fileUrl, encoding: null}, function(err, res, body) {
-            if(err) throw err;
-            fs.mkdir(directoryPrefix);
-            fs.writeFile(directoryPrefix+"/"+output, body, function(err) {
+            var fileUrl = RESOURCE_URL + options.module + ".zip";
+            var output = options.module + ".zip";
+            await downloadTemplate(fileUrl, directoryPrefix, output)
+            console.log('... download template success');
 
-                console.log('... download template success');
+            var zip = new AdmZip(directoryPrefix+"/"+output);
+            zip.extractAllTo(directoryPrefix, /*overwrite*/true);
+            await fs.unlink(directoryPrefix+"/"+output, (err) => { if (err) throw (err);});
 
-                var zip = new AdmZip(directoryPrefix+"/"+output);
-                zip.extractAllTo(directoryPrefix, /*overwrite*/true);
-                fs.unlink(directoryPrefix+"/"+output);
+            console.log('... prepare template success');
+            console.log('... start configuration');
 
-                console.log('... prepare template success');
-                console.log('... start configuration');
+            var question = await readPromptFie(process.cwd() + '/' + directoryPrefix + '/prompt.js')
+            if (question.length <= 0){
+                console.log('... no additional configuration file');
+                console.log(chalk.green("Init Project: " + projectName + " SUCCESS"));
+                return;
+            }
 
-                fs.access(directoryPrefix + '/prompt.js', fs.constants.R_OK, function (err) {
-                    if (err) {
-                        console.log('... no additional configuration file');
-                        console.log(chalk.green("Init Project: " + projectName + " SUCCESS"));
-                    }
-                    else {
-                        var prompt = require(process.cwd() + '/' + directoryPrefix + '/prompt.js')
-
-                        console.log('');
-                        inquirer.prompt(prompt).then(function (answers) {
-
-                            let content = '';
-                            for( key  in answers ){
-                                content += (key + '=' + '"' + answers[key] + '"');
-                                content += '\r\n';
-                            }
-                            console.log(content);
-
-                            fs.writeFile(directoryPrefix+'/' + projectName + '.tfvars', content, function (err) {
-                                if (err) return console.log(err);
-                                console.log('... write configuration file success');
-                                console.log(chalk.green("Init Project: " + projectName + " SUCCESS"));
-                            });
-                        });
-                    }
-                });
+            console.log('');
+            inquirer.prompt(question).then((answers) => {
+                writeTfvars(directoryPrefix+'/' + projectName + '.tfvars', answers, projectName);
             });
-        });
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
     });
 
 program
@@ -112,7 +145,7 @@ program
 //     .command('test')
 //     .description('test')
 //     .action(function(options) {
-//         console.log("Test Project:");
+//         template()
 //     });
 
 program.parse(process.argv);
